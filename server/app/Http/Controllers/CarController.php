18cd4cd4e1e8fd1ad4404a8eb3cfd3af8f69b759
malakshaer <shaermk06@gmail.com>
2022-10-11 13:00:27 +0300

    backend: register function in AuthController added

--- server/app/Http/Controllers/CarController.php
+++ server/app/Http/Controllers/CarController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Car;
 
+//Test Controller
+
 class CarController extends Controller
 {
     public function create()
@@ -20,5 +22,12 @@ class CarController extends Controller
         $car->price = $request->get('price');
         $car->save();
         return redirect('car')->with('success', 'Car has been successfully added');
+    }
+
+    public function show($slug)
+    {
+        return view('post', [
+            'post' => Car::where('slug', '=', $slug)->first()
+        ]);
     }
 }
