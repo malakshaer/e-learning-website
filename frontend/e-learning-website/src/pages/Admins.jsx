2396ea31c3799a7a0a1c9211f2a9323c760225c2
malakshaer <shaermk06@gmail.com>
2022-10-11 08:33:16 +0300

    frontend: admins page for application created

---
+++ frontend/e-learning-website/src/pages/Admins.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import AdminHeader from "../components/AdminHeader";
+import AdminPage from "../components/AdminPage";
+
+const Admins = () => {
+  return (
+    <Fragment>
+      <AdminPage />
+    </Fragment>
+  );
+};
+export default Admins;
+<AdminHeader />;
