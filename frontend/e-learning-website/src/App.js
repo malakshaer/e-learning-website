58978bf18b04fe4d2353e7ca709d0259ba308f60
malakshaer <shaermk06@gmail.com>
2022-10-11 08:29:58 +0300

    frontend: app file with route to switch between page created

---
+++ frontend/e-learning-website/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Admins from "./pages/Admins";
+import Instructors from "./pages/Instructors";
+import Students from "./pages/Students";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/Admins/*" element={<Admins />} />
+        <Route path="/Instructors " element={<Instructors />} />
+        <Route path="/Students  " element={<Students />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
