686621a0df0e5c7af611f6d49a9e02c44a6092e3
malakshaer <shaermk06@gmail.com>
2022-10-11 23:12:39 +0300

    frontend: form added tp admin page

--- frontend/e-learning-website/src/components/AdminPage.jsx
+++ frontend/e-learning-website/src/components/AdminPage.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import "../App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddButton from "./AddButton";
 
-const AdminPage = () => {
+const AdminPage = ({ text }) => {
   return (
     <div className="Admin-container">
-      <Routes></Routes>
+      <form>
+        <h3>Add Student</h3>
+        <input type={text} placeholder="Student Name"></input>
+        <input type={text} placeholder="Student email"></input>
+        <AddButton />
+      </form>
     </div>
   );
 };
