0c31ba9e699ba0f77362ec93fcee2062b0229f92
malakshaer <shaermk06@gmail.com>
2022-10-11 23:10:32 +0300

    frontend: instructor header created

---
+++ frontend/e-learning-website/src/components/InstructorHeader.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from "react";
+import "../App.css";
+
+const navLinks = [
+  {
+    display: "Assignments",
+    url: "#",
+  },
+  {
+    display: "Announcements",
+    url: "#",
+  },
+  {
+    display: "Students",
+    url: "#",
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "SignOut",
+    url: "#",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef();
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <div className="top-nav-bar">
+      <div className="nav-bar-logo">
+        <h2>LearningMS</h2>
+      </div>
+      <div>
+        <div ref={menuRef} onClick={menuToggle}>
+          <ul className="nav__list">
+            {navLinks.map((item, index) => (
+              <li key={index} className="nav__item">
+                <a href={item.url}>{item.display}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
