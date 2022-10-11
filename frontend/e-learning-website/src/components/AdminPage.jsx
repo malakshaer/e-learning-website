c1f69477ce17f68a58df5af894822869fc12f61d
malakshaer <shaermk06@gmail.com>
2022-10-11 21:20:24 +0300

    all file added

---
+++ frontend/e-learning-website/src/components/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AdminPage = () => {
+  return (
+    <div className="Admin-container">
+      <Routes></Routes>
+    </div>
+  );
+};
+
+export default AdminPage;
