adaff5840e980e051a54c7e81b4b8938569ec42f
malakshaer <shaermk06@gmail.com>
2022-10-11 23:08:37 +0300

    frontend: instructor page added

--- frontend/e-learning-website/src/pages/Instructors.jsx
+++ frontend/e-learning-website/src/pages/Instructors.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
+import InstructorHeader from "../components/InstructorHeader";
+import InstructorPage from "../components/InstructorPage";
 
 const Instructors = () => {
   return (
-    <div>
-      <p>Instructors</p>
-    </div>
+    <Fragment>
+      <InstructorHeader />;
+      <InstructorPage />
+    </Fragment>
   );
 };
-
 export default Instructors;
